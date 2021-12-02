eae75b7960089f364edbdddfaf6c6c655f47d696
afoninhere <95439629+afoninhere@users.noreply.github.com>
2021-12-02 23:17:20 +0200

    home work n2

---
+++ src/com/hillel/homework/lesson2/hw1.java
@@ -0,0 +1,15 @@
+package com.hillel.homework.lesson2;
+
+public class hw1 {
+    public static void main(String[] args) {
+
+        float myFloat = 46.437120F;
+
+        float myFloat2 = 30.758404F;
+
+        char myChar = '&';
+
+        System.out.println("My position now: " + myFloat +myChar + myFloat2);
+
+    }
+}
